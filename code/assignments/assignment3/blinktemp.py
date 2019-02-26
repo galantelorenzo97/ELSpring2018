812d898a4206bc09968e56301f0a180d23a4a07f
Lorenzo Galante <galantel1@hawkmail.newpaltz.edu>
2019-02-25 23:19:42 -0500

    SQLITE Implemented. Untested

--- code/assignments/assignment3/blinktemp.py
+++ code/assignments/assignment3/blinktemp.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
 import os
+import sqlite3
 #Assign GPIO pins
 redPin = 27
 tempPin = 17
@@ -20,6 +21,18 @@ blinkTime = 7
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(redPin,GPIO.OUT)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#SQLite setup
+db = sqlite3.connect(':memory:') #Create DB in RAM
+cursor = db.cursor() #Get cursor
+cursor.execute('''CREATE TABLE tempRecord(id INTEGER PRIMARY KEY, time TEXT, temperature TEXT)''') #Create Temperature table
+
+db.commit() #Commit Changes
+
+
+
+
+
 
 def oneBlink(pin):
 	GPIO.output(pin,True)
@@ -46,13 +59,23 @@ try:
 			if input_state == False:
 				for i in range (blinkTime):
 					oneBlink(redPin)
-				time.sleep(.2)
+				time.sleep(60)
 				data = readF(tempPin)
 				print (data)
 				log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S),str(data)))
+				timeNow = (time.strftime("%Y-%m-%d %H:%M:%S))
+				cursor.execute('''INSERT INTO tempRecord VALUES(?,?)''', (time, data))
+				db.commit()
+				all_rows = cursor.fetchall()
+				os.system('clear')
+				for row in all_rows:
+					print('{0} : {1}, {2}'.format(row[0], row[1], row[2]))
+				#END
+				
 
+				
 except KeyboardInterrupt:
 	os.system('clear')
 	print('Thanks for Blinking and Thinking!')
 	GPIO.cleanup()
-
+	db.close()
