15c7e9762f4e1c49e2b03a5d9f0c973ceab209ac
N03143948 <galantel1@hawkmail.newpaltz.edu>
2019-03-23 03:43:05 -0400

    IoT device uploaded. Gauge still not working. Debugging

---
+++ code/assignments/IoT/blinktemp.py
@@ -0,0 +1,83 @@
+#Import Libraries we will be using
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+import time
+import os
+import sqlite3
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#Assign GPIO pins
+redPin = 27
+tempPin = 17
+buttonPin = 26
+
+#Temp and Humidity Sensor
+tempSensor = Adafruit_DHT.DHT11
+#LED Variables--------------------------------------------------------
+#Duration of each Blink
+blinkDur = .1
+#Number of times to Blink the LED
+blinkTime = 7
+#---------------------------------------------------------------------
+#Initialize the GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#SQLite setup
+db = sqlite3.connect('./log/tempLog.db')
+cursor = db.cursor() #Get cursor
+
+db.commit() #Commit Changes
+
+
+
+def oneBlink(pin):
+	GPIO.output(pin,True)
+	time.sleep(blinkDur)
+	GPIO.output(pin,False)
+	time.sleep(blinkDur)
+
+def readF(tempPin):
+	humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
+	temperature = temperature * 9/5.0 +32
+	if humidity is not None and temperature is not None:
+		tempFahr = '{0:0.1f}*F'.format(temperature)
+	else:
+		print('Error Reading Sensor')
+
+	return tempFahr
+
+try:
+
+	with open("./log/templog.csv", "a") as log:
+
+		while True:
+			#BUTTON RETIRED FOR THIS ASSIGNMENT
+			#input_state = GPIO.input(buttonPin)
+			#if input_state == False:
+			for i in range (blinkTime):
+				oneBlink(redPin)
+
+			time.sleep(5)
+			data = readF(tempPin)
+			#DATA WILL BE PRINTED FROM SQL DATABASE
+			#print(data)
+			log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(data)))
+			timeNow = (time.strftime("%Y-%m-%d %H:%M:%S"))
+			cursor.execute('''INSERT INTO tempRecord VALUES(?,?)''', (timeNow, str(data)))
+			db.commit()
+			all_rows = cursor.execute('''SELECT * FROM tempRecord''')
+			os.system('clear')
+			for row in all_rows:
+				print('{0} : {1}'.format(str(row[0]), row[1],))
+			#END
+
+
+except KeyboardInterrupt:
+	os.system('clear')
+	print('Thanks for Blinking and Thinking!')
+	GPIO.cleanup()
+	db.close()
---
+++ code/assignments/IoT/flaskServer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+
+from flask import Flask, render_template, jsonify, Response
+import sqlite3
+import json
+
+app = Flask(__name__)
+
+# get most recent temp
+@app.route("/")
+def index():
+    db = sqlite3.connect('./log/tempLog.db')
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM tempRecord")
+    result = cursor.fetchone()
+    print(result[0])
+    temperature = result[0]
+    return render_template('index.html', temperature = temperature)
+
+
+# get the temp data from database
+@app.route("/catch")
+def data():
+    db = sqlite3.connect('./log/tempLog.db')
+    db.row_factory = sqlite3.Row
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM tempRecord")
+
+    entry = cursor.fetchall()
+    data = []
+    for row in entry:
+        data.append(list(row))
+
+    return Response(json.dumps(data),  mimetype='application/json')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
