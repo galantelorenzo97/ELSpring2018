7ef6e44d8a44d004b83be8927d9cce5692ca59c4
N03143948 <galantel1@hawkmail.newpaltz.edu>
2019-04-08 18:06:01 -0400

    Updated database, began implementing code for control

--- code/assignments/midterm/midterm.py
+++ code/assignments/midterm/midterm.py
@@ -8,75 +8,93 @@ import sqlite3
 import sys
 
 #Assign GPIO pins
-entrySensor = 13
-exitSensor = 26
+TRIG = 23
+ECHO = 24
 
-#Stabilization time
-stableTime = 5
+#Set door distance
+OpenMaximumDist = 15
+#Set Initial Door Sate
+doorOpen = False
 #---------------------------------------------------------------------
 #Initialize the GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(entrySensor, GPIO.IN)
-GPIO.setup(exitSensor, GPIO.IN)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
 #SQLite setup
-db = sqlite3.connect('./logPeople/peopleLog.db')
+db = sqlite3.connect('./doorLog/doorLog.db')
 cursor = db.cursor() #Get cursor
 db.commit() #Commit Changes
 
-#Using Simon's code
-
-def bothTriggers(trigger2, wait=5):
-	timeStamp = False
-	#This is the sanity check.  If the second sensor isn't triggered, it resets.
-	#The sanity check only happens while the second sensor is in a low state (not triggered)
-	timeCheck = time.time()
-	while not GPIO.input(trigger2):
-		if time.time() - timeCheck > wait:
-        		break
-		continue
-	if time.time() - timeCheck <= wait:
-        	timeStamp = time.strftime("%Y-%m-%d %H:%M:%S")
-        	time.sleep(4)
-	return timeStamp
-
 #Stabilize sensor
-time.sleep(10)
-#Initialize people in room
-peopleCount = 0
+GPIO.output(TRIG, False)
+print "Stabilizing..."
+time.sleep(3)
+print "Sensor stabilized!"
+
+#Initiate ultrasound bursts using 10us pulse
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
+
+def logToDatabase(doorState)
+	timeStamp = time.time()
+	cursor.execute('''INSERT INTO doorLog VALUES(?,?)''', (timeStamp, doorState))
+	db.commit()
+
+def getDistance()
+	while GPIO.input(ECHO)==0:
+		pulse_start = time.time()
+
+	while GPIO.input(ECHO)==1:
+		pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+
+	distance = pulse_duration * 17150
+
+	distance = round(distance, 2)
+
+	return distance
+
 
 try:
 	while True:
-		#Reset timeStamp to false to prevent writing data until both sensors are triggered again
-		timeStamp = False
-		#set entry Type
-		inOrOut = "IDLE"
-
-       		if GPIO.input(entrySensor):
-			timeStamp = bothTriggers(exitSensor)
-			if timeStamp:
-				inOrOut = "Entrance"
-				peopleCount = peopleCount + 1
-		if GPIO.input(exitSensor):
-			timeStamp = bothTriggers(entrySensor)
-			if timeStamp:
-				inOrOut = "Exit"
-				peopleCount = peopleCount - 1
+     		if not doorOpen:
+			dist = getDistance()
+			if dist < 15.0:
+				logToDatabase("open")
+				doorOpen = True
+				time.sleep(5)
+				continue
+		if doorOpen:
+			dist = getDistance()
+			if dist > 15.0:
+				logToDatabase("closed")
+				doorOpen = False
+				time.sleep(5)
+				continue
+		time.sleep(5)
 
 #Since the timeStamp is only set when the direction is determined and both sensors are triggered we use that as the condition to write our data:
-		if timeStamp:
-			cursor.execute('''INSERT INTO logPeople VALUES(?,?,?)''', (timeStamp, inOrOut, peopleCount))
-			db.commit()
-			all_rows = cursor.execute('''SELECT * FROM peopleLog''')
-			os.system('clear')
-			for row in all_rows:
-				print('{0} : {1} : {2}'.format(str(row[0]), row[1], str(row[2])))
+#		if timeStamp:
+#			db.commit()
+#			all_rows = cursor.execute('''SELECT * FROM peopleLog''')
+#			os.system('clear')
+#			for row in all_rows:
+#				print('{0} : {1} : {2}'.format(str(row[0]), row[1], str(row[2])))
+
+
+
+
+
+
 
 except db.Error, e:
 	print "Error %s:" %e.args[0]
 	sys.exit(1)
 
 except KeyboardInterrupt:
-        GPIO.cleanup()
-        db.close()
-        print('Clean exit')
+GPIO.cleanup()
+       db.close()
+       print('Clean exit')
